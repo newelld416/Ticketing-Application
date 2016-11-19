255a119bbb3b306cf06a33f570d5dececcd93095
David <dickda@mail.gvsu.edu>
2016-11-19 17:26:15 -0500

    added some logic to the speech recognition.

--- js/vio.js
+++ js/vio.js
@@ -1,7 +1,12 @@
 
-$("#speachButton").click( function()
+$("#speechButton").click( function()
    {
+	  /* for (var i = 0, len = mic.length; i < len; i++) {
+		   $("#micIMG").
+			mic[i].style.fill = "green"
+		}  */
 	 startListening()
+	 
    }
 );
 
@@ -9,21 +14,29 @@ $("#speachButton").click( function()
 function startListening(event) {
 
 	if (!('webkitSpeechRecognition' in window)) {
-	  upgrade();
+	  alert("Your browser does not support Speach Recognition.  Please try using Google Chrome 25 or later.")
 	} else {
-		
+	
 	  var final_transcript = '';
+	 
+	
+	$('#feedback').html("Talk to me");
+
+		
+	//button.style.display = "none";
 
 	  var recognition = new webkitSpeechRecognition();
-	  //recognition.lang = select_dialect.value;
+	  recognition.lang = 'en-US';
 	  recognition.start();
-	  recognition.continuous = true;
+	  recognition.continuous = false;
 	  recognition.interimResults = true;
 
-	  /*recognition.onstart = function() { ... }*/
+	  recognition.onstart = function() {
+		   $('#feedback').html("Capturing");
+		    	alert ("here1");
+	  }
 	  recognition.onresult = function(event) {
 		var interim_transcript = '';
-
 		for (var i = event.resultIndex; i < event.results.length; ++i) {
 		  if (event.results[i].isFinal) {
 			final_transcript += event.results[i][0].transcript;
@@ -32,16 +45,45 @@ function startListening(event) {
 		  }
 		}
 		
-		alert (final_transcript);
+		
+		if((final_transcript.includes("buy") || final_transcript.includes("purchase"))&& final_transcript.includes("tickets") ){
+			
+			alert("Great, let's buy some movie tickets!");
+			
+		}else{
+			alert ("Sorry, I catch what you said, I heard: /n" + final_transcript+ "/n Try saying something like \"Buy tickets\"");
+			recognition.start();
+		}
+		
+		
+		
+		$("#feedback").text(final_transcript);
 		//final_transcript = capitalize(final_transcript);
 		//final_span.innerHTML = linebreak(final_transcript);
 		//interim_span.innerHTML = linebreak(interim_transcript);
 	  };
-	/*
-	  recognition.onerror = function(event) { ... }
-	  recognition.onend = function() { ... }*/
+	
+	  //recognition.onerror = function(event) { ... }
+	  recognition.onend = function() { 
+		
+		if (final_transcript.includes("stop listening")){
+			return true;
+		}
+			sleep(1000);
+			//recognition.start();
+	  }
 
 	}
+}
+
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+	if ((new Date().getTime() - start) > milliseconds){
+	  break;
+	}
+  }
 }
 
 
